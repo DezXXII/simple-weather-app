0c736aae70d72f079dc891e26256a786a2632d01
Matias <mattkramm@hotmail.com>
2022-08-24 17:51:35 -0300

    First Commit

---
+++ scripts.js
@@ -0,0 +1,33 @@
+let weather = {
+    "apiKey": "282bcd6a6ebbe1518567263ae5c480ae",
+    fetchWeather: function(city) {
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' 
+        + city 
+        + '&units=metric&appid=' 
+        + this.apiKey)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = Math.round(temp) + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind: " + speed + "km/h";
+    }
+}
+
+const inputCity = document.getElementById("inputCity");
+const btnQuery = document.getElementById("btnQuery");
+
+btnQuery.addEventListener('click', (e) => {
+
+    weather.fetchWeather(inputCity.value);
+
+});
